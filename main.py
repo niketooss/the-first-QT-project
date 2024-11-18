0013ee1dd64601419f2c81e9a743d2739a0b4304
niketooss <niketooss27@gmail.com>
2024-11-18 20:22:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,24 +23,11 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
    <string>MainWindow</string>
   </property>
   <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="pushButton">
-    <property name="geometry">
-     <rect>
-      <x>250</x>
-      <y>260</y>
-      <width>151</width>
-      <height>23</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Зарегистрироваться</string>
-    </property>
-   </widget>
    <widget class="QLabel" name="label_4">
     <property name="geometry">
      <rect>
-      <x>390</x>
-      <y>410</y>
+      <x>372</x>
+      <y>385</y>
       <width>101</width>
       <height>16</height>
      </rect>
@@ -49,47 +36,34 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
      <string>Зарегистрированы?</string>
     </property>
    </widget>
-   <widget class="QPushButton" name="pushButton_2">
+   <widget class="QWidget" name="layoutWidget">
     <property name="geometry">
      <rect>
-      <x>394</x>
-      <y>440</y>
-      <width>91</width>
-      <height>23</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Войти</string>
-    </property>
-   </widget>
-   <widget class="QWidget" name="">
-    <property name="geometry">
-     <rect>
-      <x>158</x>
-      <y>145</y>
+      <x>140</x>
+      <y>120</y>
       <width>331</width>
       <height>101</height>
      </rect>
     </property>
-    <layout class="QHBoxLayout" name="horizontalLayout">
+    <layout class="QHBoxLayout" name="horizontalLayout_3">
      <item>
-      <layout class="QVBoxLayout" name="verticalLayout">
+      <layout class="QVBoxLayout" name="verticalLayout_5">
        <item>
-        <widget class="QLabel" name="label">
+        <widget class="QLabel" name="label_8">
          <property name="text">
           <string>Имя</string>
          </property>
         </widget>
        </item>
        <item>
-        <widget class="QLabel" name="label_2">
+        <widget class="QLabel" name="label_9">
          <property name="text">
           <string>Логин</string>
          </property>
         </widget>
        </item>
        <item>
-        <widget class="QLabel" name="label_3">
+        <widget class="QLabel" name="label_10">
          <property name="text">
           <string>Пароль</string>
          </property>
@@ -98,20 +72,56 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
       </layout>
      </item>
      <item>
-      <layout class="QVBoxLayout" name="verticalLayout_2">
+      <layout class="QVBoxLayout" name="verticalLayout_6">
        <item>
-        <widget class="QLineEdit" name="NameEnter"/>
+        <widget class="QLineEdit" name="NameLineEdit_3"/>
        </item>
        <item>
-        <widget class="QLineEdit" name="LoginEnter"/>
+        <widget class="QLineEdit" name="LoginLineEdit_3"/>
        </item>
        <item>
-        <widget class="QLineEdit" name="PasswordEnter"/>
+        <widget class="QLineEdit" name="PasswordLineEdit_3"/>
        </item>
       </layout>
      </item>
     </layout>
    </widget>
+   <widget class="QPushButton" name="pushButton">
+    <property name="geometry">
+     <rect>
+      <x>232</x>
+      <y>235</y>
+      <width>151</width>
+      <height>23</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Зарегистрироваться</string>
+    </property>
+   </widget>
+   <widget class="QPushButton" name="pushButton_2">
+    <property name="geometry">
+     <rect>
+      <x>376</x>
+      <y>415</y>
+      <width>91</width>
+      <height>23</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Войти</string>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>650</width>
+     <height>21</height>
+    </rect>
+   </property>
   </widget>
   <widget class="QStatusBar" name="statusbar"/>
  </widget>
@@ -134,6 +144,13 @@ class Main(QWidget):
         self.NameEnter = QLineEdit(self)
         self.LoginEnter = QLineEdit(self)
         self.PasswordEnter = QLineEdit(self)
+
+
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.saveButton.clicked.connect(self.save_results)
+        self.modified = {}
+        self.titles = None
 
     def initUI(self):
         '''Настройка интерфейса'''
